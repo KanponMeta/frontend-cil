691775676f484b4ba486ab04f9f70081b166ee10
kanpon <kanpon@yeah.net>
2023-05-24 15:04:46 +0800

    docs: scripts脚本名称修改

--- template/config/deploy/scripts/deployConfig.js
+++ template/config/deploy/scripts/deployConfig.js
@@ -1,9 +1,9 @@
 const devConfig = {
-  ip: '192.168.1.xx',
+  ip: '150.158.82.12',
   port: '22',
-  username: 'huolala',
-  password: '123456',
-  remotePath: '/home/huolala/dist'
+  username: 'root',
+  password: '******',
+  remotePath: '/data/wwwroot/www.kanpon.com/vue/dataportal'
 }
 const prodConfig = {
   ip: '192.168.1.xx',
